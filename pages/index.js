3c332c24c352695b32f9deb65a314644984ec776
cagrigit-hub <77447064+cagrigit-hub@users.noreply.github.com>
2022-06-12 03:31:15 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,64 +9,7 @@ import Footer from "../components/Footer";
 const greenItemAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 export default function Home() {
-  const info = [
-    {
-    "id" :0,
-    "name":"Green Level 1",
-    "image":"https://gateway.pinata.cloud/ipfs/Qmc3joRdy6naBJcFW4y68sNFbhM7MX3kSHVs3przBEcBf6"
-    
-    },
-    {
-    "id" :1,
-    "name":"Yellow Level 1",
-    "image":"https://gateway.pinata.cloud/ipfs/QmTfXmKKckcAV5pWZ5SK7SqarPhCJbhwoKtVdzXoUS4p4V"
-    
-    },
-    {
-    "id" :2,
-    "name":"Blue Level 1",
-    "image":"https://gateway.pinata.cloud/ipfs/QmdrMsxS2rhudZVf3HEDNQGzwfMBQzkYzqKDk3QTwRgSKC"
-    
-    },
-    {
-    "id" :3,
-    "name":"Green Level 2",
-    "image":"https://gateway.pinata.cloud/ipfs/QmUi42buR65ufHHxrYyaoUMEpZ9yx4dkLpfPsubayujgcX"
-    
-    },
-    {
-    "id" :4,
-    "name":"Yellow Level 2",
-    "image":"https://gateway.pinata.cloud/ipfs/QmWGxTkQbZhhSCaSfwtpXC97MucJ1MnQhuxSt6rSVNidB8"
-    
-    },
-    {
-    "id" :5,
-    "name":"Blue Level 2",
-    "image":"https://gateway.pinata.cloud/ipfs/Qmb8Ra3d3mUQsW1PFkyjSTiYj2ekp5LxTw68aVfCS4BDyC"
-    
-    },
-    {
-    "id" :6,
-    "name":"Green Level 3",
-    "image":"https://gateway.pinata.cloud/ipfs/QmeKus3kQeBBWNARCzxcBjHgfTAsCwEKqShKnjHeySnNpy"
-    
-    },{
-    "id" :7,
-    "name":"Yellow Level 3",
-    "image":"https://gateway.pinata.cloud/ipfs/Qmcip9AnHyUkVQozzkTMei6THxxkW5MB67ZYkeyJr4prGh"
-    
-    },{
-    "id" :8,
-    "name":"Blue Level 3",
-    "image":"https://gateway.pinata.cloud/ipfs/QmXLBEvfVGYpZobe4tdHX6rvPMbJcGH2vK4i9hqbZRsEZM"
-    
-    },{
-    "id" :9,
-    "name":"Ultimate Recycler",
-    "image":"https://gateway.pinata.cloud/ipfs/QmY8DezUfCW9aoenMa1VkmrSz1LFNKWbUxDUdYHq7EPjgq"
-    
-    }]
+  
   const [user,setWalletAddress] = useState();
   async function requestAccount() {
     console.log('Requesting account...');
@@ -111,7 +54,7 @@ export default function Home() {
       <>
       <NavBar user={user} />
       <Recycle addr={greenItemAddress} abi={GreenItem.abi} />
-      <Balance user={user} addr={greenItemAddress} abi={GreenItem.abi} info={info} />
+      <Balance user={user} addr={greenItemAddress} abi={GreenItem.abi} />
    
       <Footer />
       </>
